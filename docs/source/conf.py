4c10111e4d104b16a9324340deb9f2a3915d7d12
Helmut Hoffer von Ankershoffen <helmut@aignostics.com>
2025-01-02 12:08:51 +0100

    docs(sphinx,readthedocs): introduce

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ extensions = [
     "sphinx_copybutton",
     "sphinx.ext.coverage",
     "sphinx_mdinclude",
+    "sphinx_rtd_dark_mode",
 ]
 
 templates_path = ["_templates"]
@@ -33,5 +34,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
